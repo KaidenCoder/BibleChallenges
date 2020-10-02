f2a81eed0a917f0c64a504166121212a3173892b
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-02 16:51:28 +0530

    del

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,16 +3,6 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        // <div style={{ textAlign: "center", justifyContent: "center" }}>
-        //     <ul className="nav center mx-auto">
-        //         {/* <li className="nav-item">
-        //             <a className="nav-link active" href="/">30DaysChallenge</a>
-        //         </li> */}
-        //         <li className="nav-link"><NavLink exact to="/">30DaysChallenge</NavLink></li>
-        //         <li className="nav-link"><NavLink exact to="/">60DaysChallenge</NavLink></li>
-        //         <li className="nav-link"><NavLink exact to="/">About</NavLink></li>
-        //     </ul>
-        // </div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light center">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
