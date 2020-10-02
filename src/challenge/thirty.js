c55e62ccafce93e797febc52ccbe7b3bd0ce7dea
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-02 16:55:24 +0530

    star

--- src/challenge/thirty.js
+++ src/challenge/thirty.js
@@ -5,9 +5,9 @@ const Thirty = () => {
     return (
         <React.Fragment>
             <div style={{ background: "rgba(0,0,255,0.4)", margin: '0', padding: '40px' }}>
-                <h2 style={{ textAlign: "center" }}>The #30DaysChallenge Honorable Mentions Wall</h2>
+                <h2 style={{ textAlign: "center" }}>🌟The #30DaysChallenge Honorable Mentions Wall🌟</h2>
                 <br />
-                <p style={{ textAlign: "center" }}>The following names who have completed the challenge</p>
+                <p style={{ textAlign: "center" }}>The following names who have completed the challenge🌟</p>
 
                 <p style={{ textAlign: "center" }}>Loading...</p>
             </div>
