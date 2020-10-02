29e9212e85fc49f4e2a962bf525f29e307fcb8b5
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-10-02 16:04:05 +0530

    Second commit

--- src/challenge/thirty.js
+++ src/challenge/thirty.js
@@ -5,9 +5,9 @@ const Thirty = () => {
     return (
         <>
             <div style={{ background: "rgba(0,0,255,0.4)", margin: '0', padding: '40px' }}>
-                <h2 style={{ textAlign: "center" }}>🌟 The #30DaysChallenge Honorable Mentions Wall 🌟</h2>
+                <h2 style={{ textAlign: "center" }}>The #30DaysChallenge Honorable Mentions Wall</h2>
                 <br />
-                <p style={{ textAlign: "center" }}>The following names who have completed the challenge 🌟</p>
+                <p style={{ textAlign: "center" }}>The following names who have completed the challenge</p>
 
                 <p style={{ textAlign: "center" }}>Loading...</p>
             </div>
