2a3c568d652f25e82c417c9f867490e5c5ad5eef
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2021-05-10 11:46:52 +0530

    year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer-copyright text-center py-3">
 
-            © 2020 Copyright: Maintained by K
+            © 2021 Copyright: Maintained by K
 
         </div>
     )
