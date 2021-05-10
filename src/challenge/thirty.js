2152aa6b2bf89dc32ac1dfa7e20d277026ac6c93
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2021-05-10 22:18:48 +0530

    style

--- src/challenge/thirty.js
+++ src/challenge/thirty.js
@@ -5,26 +5,28 @@ import Reviews from './Reviews'
 const Thirty = () => {
     return (
         <React.Fragment>
-            <div style={{ background: "#6EBEF7", margin: '0', padding: '40px' }}>
-                <h2 style={{ textAlign: "center", color: "rgba(0,0,0,0.8)" }}>The #30DaysBibleChallenge Honorable Mentions Wall</h2>
+
+            <div style={{ background: "rgba(110, 190, 247, 1)", margin: '0', padding: '40px' }}>
+                <h2 style={{ textAlign: "center", color: "rgba(0,0,0,0.8)" }}>Honourable Mentions Wall</h2>
                 <br />
-                <p style={{ textAlign: "center", color: "rgba(0,0,0,0.8)" }}>The following names who have completed the challenge</p>
-                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,0.8)", color: "rgba(0,0,0,0.7)" }}>
-                    <div className="col px-md-5"><div className="p-2 border-none ">Khaidem Sandip</div></div>
-                    <div className="col px-md-5"><div className="p-2 border-none ">Julia Kamei</div></div>
-                    <div className="col px-md-5"><div className="p-2 border-none ">Prescilla Singha</div></div>
-                </div>
-                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,0.8)", color: "rgba(0,0,0,0.7)" }}>
+                <p style={{ textAlign: "center", color: "rgba(0,0,0,1)" }}>The following names who have completed the challenge</p>
+
+                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,1)", color: "rgba(0,0,0,1)", padding: "40px 0px 0 0", borderRadius: "1px" }}>
                     <div className="col px-md-5"><div className="p-2 border-none ">Vision Hanse</div></div>
-                    <div className="col px-md-5"><div className="p-2 border-none ">MT Sungte</div></div>
+                    <div className="col px-md-5"><div className="p-2 border-none ">Julia Kamei</div></div>
                     <div className="col px-md-5"><div className="p-2 border-none ">Kago Kanya</div></div>
                 </div>
-                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,0.8)", color: "rgba(0,0,0,0.7)" }}>
+                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,1)", color: "rgba(0,0,0,1)" }}>
                     <div className="col px-md-5"><div className="p-2 border-none ">G Sarah Rongmei</div></div>
                     <div className="col px-md-5"><div className="p-2 border-none ">Sohiam</div></div>
                     <div className="col px-md-5"><div className="p-2 border-none ">Jessica Gangmei</div></div>
                 </div>
-                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,0.8)", color: "rgba(0,0,0,0.7)" }}>
+                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,1)", color: "rgba(0,0,0,1)" }}>
+                    <div className="col px-md-5"><div className="p-2 border-none ">Khaidem Sandip</div></div>
+                    <div className="col px-md-5"><div className="p-2 border-none ">MT Sungte</div></div>
+                    <div className="col px-md-5"><div className="p-2 border-none ">Prescilla Singha</div></div>
+                </div>
+                <div className="row mx-md-n3" style={{ background: "rgba(255,255,255,1)", color: "rgba(0,0,0,1)", padding: "0 0 40px 0", borderRadius: "1px" }}>
                     <div className="col px-md-5"><div className="p-2 border-none ">G. Elizabeth Rongmei</div></div>
                     <div className="col px-md-5"><div className="p-2 border-none "></div></div>
                     <div className="col px-md-5"><div className="p-2 border-none "></div></div>
